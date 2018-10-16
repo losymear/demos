3166e146c5dfb87d13bf22cf03b04490116df69a
losymear <w15868821768@gmail.com>
2018-10-17 02:26:24 +0800

    asyncio synchronization primitive demo

--- src/demos/network/asyncio_demo/asyncio_synchronization_demo.py
+++ src/demos/network/asyncio_demo/asyncio_synchronization_demo.py
@@ -7,7 +7,6 @@
 
 
 import asyncio
-import datetime
 import time
 
 
@@ -29,7 +28,7 @@ class LockTest:
 
 
 class EventTest:
-    # 不能在__init__设置event值，否则会报
+    # cannot set event in__init__, or will raise an error:
     # got Future <Future pending> attached to a different loop
     event: asyncio.Event
 
@@ -87,9 +86,49 @@ class ConditionTest:
         await boy_task
 
 
+class SemaphoreTest:
+    semaphore: asyncio.Semaphore
+
+    async def worker(self, sleep_time: int, i: int) -> None:
+        await asyncio.sleep(sleep_time)
+        await self.semaphore.acquire()
+        print("worker {} sleep {}s {}".format(sleep_time, i, time.strftime("%X")))
+
+    async def main(self):
+        self.semaphore = asyncio.Semaphore(3)
+        print('{message:30} {time:s}'.format(message="starting workers...", time=time.strftime("%X")))
+        worker0 = asyncio.create_task(self.worker(0, 0))
+        worker1 = asyncio.create_task(self.worker(1, 1))
+        worker2 = asyncio.create_task(self.worker(2, 2))
+        # run 3 tasks concurrently
+        await asyncio.gather(worker0, worker1, worker2)
+        # semaphore now is locked
+        print("semaphore is locked:", self.semaphore.locked())
+
+
 print("start testing lock")
 asyncio.run(LockTest().main())
+# waiting for event...           02:33:03
+# ... got lock after 2s! ...     02:33:05
+
 print("\n\nstart testing event")
 asyncio.run(EventTest().main())
+# waiting for event...           02:33:05
+# ... event set after 3s!...     02:33:08
+
 print("\n\nUse Condition to make a dialog, in turn to notify() and wait")
 asyncio.run(ConditionTest().main())
+# Hay girl, I'm Nhor
+# Hey Nhor, I'm Amy
+# Nice to meet you, Amy
+# Nice to meet you too, Nhor
+# Amy, you look so fat!
+# Well, you're really like a dirty pig I've seen. So amazing!
+
+print("\n\nstart testing semaphore")
+asyncio.run(SemaphoreTest().main())
+# starting workers...            02:33:08
+# worker 0 sleep 0s 02:33:08
+# worker 1 sleep 1s 02:33:09
+# worker 2 sleep 2s 02:33:10
+# semaphore is locked: True
